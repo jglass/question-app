d612603b905d5110346360cc70613f25271e0134
john <john@dailykos.com>
2022-08-19 13:37:50 -0700

    Move data to an object in a separate file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { useState } from 'react';
 import Question from './Question';
+import InitialData from './InitialData';
 
 function App(props) {
+  let questionsArray = [InitialData.questionA,
+                        InitialData.questionB,
+                        InitialData.questionC,
+                        InitialData.questionD];
   const [selectedValue, setSelectedValue] = useState("");
+  const [questionsValue, setQuestionsValue] = useState(questionsArray);
   function onChange(e) {
     setSelectedValue(e.target.value);
   }
 
+  function resetForm(e) {
+    setSelectedValue("");
+    setQuestionsValue(questionsArray);
+    var radio = document.querySelector('input[type=radio][name=question]:checked');
+    if(radio) { radio.checked = false; }
+  }
+
+  function formSubmit(event) {
+    event.preventDefault();
+        switch(selectedValue) {
+          case "Question A":
+            setQuestionsValue(["Question E", "Question F", "Question G", "Question H"]);
+            break;
+          case "Question B":
+            setQuestionsValue(["Question I", "Question J", "Question K", "Question L"]);
+            break;
+            case "Question C":
+              setQuestionsValue(["Question M", "Question N", "Question O", "Question P"]);
+             break;
+           case "Question D":
+             setQuestionsValue(["Question Q", "Question R", "Question S", "Question T"]);
+             break;
+          default:
+            setQuestionsValue(["Question A", "Question B", "Question C", "Question D"]);
+            break;
+        }
+        var radio = document.querySelector('input[type=radio][name=question]:checked');
+        if(radio) { radio.checked = false; }
+  }
+
   return (
     <div>
-      <Question question={props.QuestionOne} onChange={onChange} />
-      <Question question={props.QuestionTwo} onChange={onChange} />
-      <Question question={props.QuestionThree} onChange={onChange} />
-      <Question question={props.QuestionFour} onChange={onChange} />
-      Selected value is {selectedValue}
+      <form onSubmit={formSubmit}>
+        <Question question={questionsValue[0].questionText} onChange={onChange} />
+        <Question question={questionsValue[1].questionText} onChange={onChange} />
+        <Question question={questionsValue[2].questionText} onChange={onChange} />
+        <Question question={questionsValue[3].questionText} onChange={onChange} />
+        <button type="submit">Submit</button>
+        <button onClick={resetForm}>Reset</button>
+      </form>
+        Selected value is {selectedValue}
     </div>
   );
 }
---
+++ src/InitialData.js
@@ -0,0 +1,64 @@
+const InitialData = {
+  questionA:
+    { questionValue: "Question A",
+    questionText: "Reggae",
+    nextChoices: [
+        { questionValue: "Question E",
+        questionText: "Bob Marley",
+        nextChoices: {
+          recommendation: "Peter Tosh",
+        }},
+        { questionValue: "Question F",
+        questionText: "Ziggy Marley",
+        nextChoices: {
+          recommendation: "Miriam Makeba",
+        }},
+        { questionValue: "Question G",
+        questionText: "Jimmy Cliff",
+        nextChoices: {
+          recommendation: "Burning Spear",
+        }},
+        { questionValue: "Question H",
+        questionText: "Lee 'Scratch' Perry",
+        nextChoices: {
+          recommendation: "UB40",
+        }},
+    ] },
+  questionB:
+    { questionValue: "Question B",
+      questionText: "Rock",
+    nextChoices: [
+        { questionValue: "Question E",
+        questionText: "Beatles",
+        nextChoices: {
+          recommendation: "Wilco",
+        }},
+          { questionValue: "Question F",
+          questionText: "Pink Floyd",
+          nextChoices: {
+            recommendation: "Primus",
+          }},
+          { questionValue: "Question G",
+          questionText: "Rolling Stones",
+          nextChoices: {
+            recommendation: "Red Hot Chili Peppers",
+          }},
+          { questionValue: "Question H",
+          questionText: "The Who",
+          nextChoices: {
+            recommendation: "Radiohead",
+          }},
+       ]
+    },
+  questionC:
+    { questionValue: "Question C",
+      questionText: "Jazz",
+      step: 1 },
+  questionD:
+    { questionValue: "Question D",
+      questionText: "Classical",
+      step: 1 },
+
+};
+
+export default InitialData;
