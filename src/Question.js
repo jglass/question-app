da069179934ed2898b6d89ea42ac556c67a4372a
john <john@dailykos.com>
2022-09-09 14:05:10 -0700

    Display choices evenly with flex

--- src/Question.js
+++ src/Question.js
@@ -4,15 +4,14 @@ function Question(props) {
   return(
     <a href="#" onClick={() => { props.onChange(props.question) } } >
       <li>
-        <img src={props.imageUrl}
-             height="125"
-             width="125"
-             value={props.question} />
-
+        <figure>
+          <img src={props.imageUrl}
+               height="125"
+               width="125"
+               value={props.question} />
+          <figcaption>{props.question}</figcaption>
+        </figure>
       </li>
-      <label>
-       {props.question}
-      </label>
     </a>
   );
 }
