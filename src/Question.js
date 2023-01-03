721fe741419e371b4aa01c65262e8205c99af9d7
john <john@dailykos.com>
2023-01-03 14:06:35 -0800

    Remove unneeded player from question component

--- src/Question.js
+++ src/Question.js
@@ -17,9 +17,6 @@ function Question(props) {
                alt={props.question} />
           <figcaption>{props.question}</figcaption>
         </figure>
-        {props.trackId &&
-          <iframe style={{ borderRadius: "12px" }} src={`https://open.spotify.com/embed/track/${props.trackId}`} width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-        }
       </li>
     </a>
   );
