e0e163c4fddef2400394840766864d7d791e580a
john <john@dailykos.com>
2023-01-24 14:20:22 -0800

    Remove tests as they will be moved to components

--- src/App.test.js
+++ src/App.test.js
@@ -13,28 +13,33 @@ describe('rendering', () => {
     firstQuestionLink = screen.getByTestId(/Question A/i);
   });
 
-  it('renders first link', () => {
+  it('renders genre question', () => {
     expect(firstQuestionLink).toBeTruthy();
   });
 
-  it('renders 2nd screen', () => {
-    fireEvent.click(firstQuestionLink);
-    const secondQuestionLink = screen.getByTestId(/Question E/i);
-    expect(secondQuestionLink).toBeTruthy();
-  });
-
-  it('renders answer screen', () => {
-    fireEvent.click(firstQuestionLink);
-    const secondQuestionLink = screen.getByTestId(/Question E/i);
-    fireEvent.click(secondQuestionLink);
-    const answerElement = screen.getByText(/Peter Tosh/i);
-    expect(answerElement).toBeTruthy();
-  });
-
-  it('resets if button is pressed', () => {
-    fireEvent.click(firstQuestionLink);
-    const secondQuestionLink = screen.getByTestId(/Question E/i);
-    fireEvent.click(screen.getByText(/Reset/i));
-    expect(firstQuestionLink).toBeTruthy();
-  });
+  // it('renders text questions', () => {
+  //   fireEvent.click(firstQuestionLink);
+  //   const secondQuestionLink = screen.getByTestId(/1A/i);
+  //   expect(secondQuestionLink).toBeTruthy();
+  //   fireEvent.click(secondQuestionLink);
+  //   const thirdQuestionLink = screen.getByTestId(/2A/i);
+  //   expect(thirdQuestionLink).toBeTruthy();
+  // });
+  //
+  // it('renders answer screen', () => {
+  //   fireEvent.click(firstQuestionLink);
+  //   const secondQuestionLink = screen.getByTestId(/1A/i);
+  //   fireEvent.click(secondQuestionLink);
+  //   const answerElement = screen.getByText(/Peter Tosh/i);
+  //   fireEvent.click(secondQuestionLink);
+  //   const thirdQuestionLink = screen.getByTestId(/2A/i);
+  //   fireEvent.click(thirdQuestionLink);
+  // });
+  //
+  // it('resets if button is pressed', () => {
+  //   fireEvent.click(firstQuestionLink);
+  //   const secondQuestionLink = screen.getByTestId(/Question E/i);
+  //   fireEvent.click(screen.getByText(/Reset/i));
+  //   expect(firstQuestionLink).toBeTruthy();
+  // });
 });
