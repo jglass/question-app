c08dc99843e7b728f7d38b63842d653d7b41ba05
john <john@dailykos.com>
2023-02-14 12:41:01 -0800

    Move tests to test folder

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 describe('rendering', () => {
   let firstQuestionLink;
